08c092c856b633444bcd53fe16c32ffe29f2ca8c
Dan Rosu <cristian.rosu@student.upt.ro>
2022-05-17 16:12:58 +0300

    SCHOOL-10 <Search user in  database>

--- src/main/java/org/loose/fis/sre/controllers/LoginController.java
+++ src/main/java/org/loose/fis/sre/controllers/LoginController.java
@@ -1,11 +1,25 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.services.UserService;
+
+import java.io.IOException;
 
 public class LoginController {
 
+    @FXML
+    private Label lbl;
     @FXML
     private TextField name;
 
@@ -14,9 +28,28 @@ public class LoginController {
 
     @FXML
     private ChoiceBox role;
+    public ObjectRepository<User> user;
+    public User currentUser;
 
     public void initialize() {
         role.getItems().addAll("Student", "Teacher");
+    }
+    public void login(ActionEvent event) throws IOException {
+        user = UserService.getDatabase();
+        for(User u : user.find() ){
+            if(name.getText().equals(u.getUsername()) &&  UserService.encodePassword(name.getText(),pass.getText()).equals(pass.getText())){
+             currentUser = u;
+             Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("UserPage.fxml"));
+              Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+              Scene scene = new Scene(root);
+              stage.setScene(scene);
+              stage.setTitle("School App");
+              stage.show();
+            }
+            else{
+                lbl.setText(" Invalid credentials !!! ");
+            }
+        }
     }
 
 }
